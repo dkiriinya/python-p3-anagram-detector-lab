c0826a5d7a22cbc53be5a07a4ff62108bade9a59
dkiriinya <dkiriinya06sc@gmail.com>
2024-03-01 12:09:14 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self,word):
+        self.word = word
+    
+    @property
+    def word(self):
+        return self.__word 
+    
+    @word.setter
+    def word(self,word):
+        self._word = word
+        
+    def match(self, arr):
+        word_arr = sorted(self._word.lower())
+        anagrams = [word for word in arr if sorted(word.lower()) == word_arr and word.lower() != self._word.lower()]
+        return anagrams
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
